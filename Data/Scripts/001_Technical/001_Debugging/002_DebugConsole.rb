39a171135aa60c6a5cd0efae503e16d5b68653e6
Golisopod-User <pkmnmonsieur@gmail.com>
2021-05-05 12:26:59 +0530

    Update 002_DebugConsole.rb

--- Data/Scripts/001_Technical/001_Debugging/002_DebugConsole.rb
+++ Data/Scripts/001_Technical/001_Debugging/002_DebugConsole.rb
@@ -4,6 +4,7 @@
 module Console
   def self.setup_console
     return unless $DEBUG
+    echoln "\nOpenGL Texture Size : #{Bitmap.max_size}"
     echoln "--------------------------------"
     echoln "#{System.game_title} Output Window"
     echoln "--------------------------------"
@@ -16,6 +17,7 @@ module Console
     echoln "you want to get rid of this window, run the"
     echoln "program from the Shell, or download a Release"
     echoln "version."
+    echoln "(Try not to exceed the height of #{Bitmap.max_size}px\nwhen making a tileset.\n"
     echoln ""
     echoln "--------------------------------"
     echoln "Debug Output:"
